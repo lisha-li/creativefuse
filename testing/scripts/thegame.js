8fb4e7bb7ae0aa26c7df56eeb158c596f594ea7a
Lisha Li <lili2416@uni.sydney.edu.au>
2015-10-20 12:44:52 +1100

    Test

--- testing/scripts/thegame.js
+++ testing/scripts/thegame.js
@@ -12,11 +12,29 @@ jQuery(document).ready(function(){
 			selCode = selCode[1].split('"');
 			var intVal = parseInt(selCode[0]);
 			
-			$selected.prevAll().css("background","none");
-			$selected.nextAll().css("background","none");
-			$selected.css("background","url('graphics/Ingredients/"+intVal+"a.png')");
+			if ($selected.find("#i1").length > 0) {
+				$('#buzzer').get(0).play();
+			}
 			
-			if ($(".fuse_list li").length) {
+			if( $(".fuse_list li").length > 10) {
+				$("#popupError2").fadeIn();
+				$("body").append('<div id="fade"></div>');
+				$("#fade").css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+				
+				var popuptopmargin = ($("#popupError2").height() + 10) / 2;
+				var popupleftmargin = ($("#popupError2").width() + 10) / 2;
+				
+				$("#popupError2").css({
+					'margin-top' : -popuptopmargin,
+					'margin-left' : -popupleftmargin
+				});
+						
+				$("#popupOk,#fade").click(function() {
+					$("#fade , #popupError2").fadeOut()
+					return false;
+				});
+			}
+			else if ($(".fuse_list li").length) {
 				$(".fuse_list").append("<li><h3 class='plus'>+</h3></li><li><img src='graphics/Ingredients/"+intVal+"a.png'><input class='ingredient' value='"+intVal+"'></li>");
 			}
 			else {
@@ -26,13 +44,12 @@ jQuery(document).ready(function(){
 		},
 		angleOffset: 0 // in degrees
 	});
-	
 			
 	addEventListener('load',function(){
 		jQuery("#radial_container,#radial_container2,#radial_container3,#radial_container4").radmenu("show");
 	},
 	false);
-			
+	
 	$("#your_fuse").on('click', 'li', function() {
 		if ($(this).is(':first-child')){
 			$(this).next().remove();
@@ -62,7 +79,48 @@ jQuery(document).ready(function(){
 		showLayer('page3');
 	});
 	$("#fuse").click(function() {
-		showValues(this);
+		
+		if($(".fuse_list li").length > 2) {
+	
+			$("#popupSubmit").fadeIn();
+			$("body").append('<div id="fade"></div>');
+			$("#fade").css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+			
+			var popuptopmargin = ($("#popupSubmit").height() + 10) / 2;
+			var popupleftmargin = ($("#popupSubmit").width() + 10) / 2;
+			
+			$("#popupSubmit").css({
+				'margin-top' : -popuptopmargin,
+				'margin-left' : -popupleftmargin
+			});
+						
+			$("#popupNo,#fade").click(function() {
+				$("#fade , #popupSubmit").fadeOut();
+			});
+			
+			$("#popupYes2").click(function() {
+				$("#fade , #popupSubmit").fadeOut();
+				showValues(this);
+			});
+		}
+		else {
+			$("#popupError").fadeIn();
+			$("body").append('<div id="fade"></div>');
+			$("#fade").css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+			
+			var popuptopmargin = ($("#popupError").height() + 10) / 2;
+			var popupleftmargin = ($("#popupError").width() + 10) / 2;
+			
+			$("#popupError").css({
+				'margin-top' : -popuptopmargin,
+				'margin-left' : -popupleftmargin
+			});
+					
+			$("#popupOk,#fade").click(function() {
+				$("#fade , #popupError").fadeOut()
+				return false;
+			});
+		}
 	});
 			
 	var currentLayer = 'page1';
@@ -95,8 +153,8 @@ jQuery(document).ready(function(){
 		$("#combo").val(comboVal);
 		if(typeof comboVal != "undefined" && comboVal != null && comboVal.length > 1){
 			var thisCombo = comboVal.slice().sort(function(a, b){return a-b});
-			var drinkCombos = [[0,4,16,17],[0,10,15],[0,8,10],[1,13,14],[1,5,8,13,14,18],[7,12,15,17,20],[7,10,12],[0,3,8,14,18],[3,8,18],[3,5,10,15],[2,8,12,18],[2,12,17,20],[8,12,15,17,18,20],[10,12,15,17,20],[16,17]];
-			var drinklist = ["Expresso Martini", "Cranberry Kick", "Madras", "Pina Colada", "Mai Tai", "London Lemonade", "Cranberry Gin", "Morton's Special", "Tequila Sunrise", "Trotsky", "Divan", "John Collins", "Shirley Temple", "Gentle Sea Breeze", "Ice Coffee"];
+			var drinkCombos = [[1,5,17,18],[1,11,16],[1,9,11],[2,15,24],[2,6,9,15,19,24],[8,13,16,18,21],[8,11,13],[1,4,9,15,19],[4,9,19],[4,6,11,16],[3,9,13,19],[3,13,18,21],[9,13,16,18,19,21],[11,13,16,18,21],[17,18]];
+			var drinklist = ["ExpressoMartini", "CranberryKick", "Madras", "PinaColada", "MaiTai", "LondonLemonade", "CranberryGin", "MortonsSpecial", "TequilaSunrise", "Trotsky", "Divan", "JohnCollins", "ShirleyTemple", "GentleSeaBreeze", "IceCoffee"];
 			var combo = "new";
 			var drink = "New";
 			for (i = 0; i < drinkCombos.length; ++i){
@@ -106,21 +164,17 @@ jQuery(document).ready(function(){
 					break;
 				};
 			};
-			//window.location = combo+'Combo.html?drink='+drink+'&combo='+comboVal
 			
 			if (combo == "new") {
+				$('#wrong').get(0).play();
 				$("#newFuse").html(fuse);
+				$("#newFuse").append("<li><h3 class='plus'>=</h3></li><li><img class='beaker' src='graphics/Icons/beaker.png' /></li>");
 				$("#popupNew").fadeIn();
-				
-				$("#popupNewOk").click(function() {
-					$("#popupNew").fadeOut();
-					
-					$("#popupWarn").fadeIn();
-				});
 			}
 			else if(combo == "win"){
+				$('#winner').get(0).play();
 				$("#winFuse").html(fuse);
-				$(".drinkImg").attr("src","../graphics/"+drink+".png");
+				$(".drinkImg").attr("src","graphics/Drinks/"+drink+".png");
 				$("#popupWin").fadeIn();
 				
 				$("#popupWinOk").click(function() {
@@ -130,23 +184,5 @@ jQuery(document).ready(function(){
 				});
 			}
 		}
-		else {
-			$("#popupError").fadeIn();
-			$("body").append('<div id="fade"></div>');
-			$("#fade").css({'filter' : 'alpha(opacity=80)'}).fadeIn();
-			
-			var popuptopmargin = ($("#popupError").height() + 10) / 2;
-			var popupleftmargin = ($("#popupError").width() + 10) / 2;
-			
-			$("#popupError").css({
-				'margin-top' : -popuptopmargin,
-				'margin-left' : -popupleftmargin
-			});
-					
-			$("#popupOk,#fade").click(function() {
-				$("#fade , #popupError").fadeOut()
-				return false;
-			});
-		};
 	};
 });
